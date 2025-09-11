c29223632455a2e4633c570f53ee754a7fd97e35
dusta <dusta@DESKTOP-ORJ6J08>
2025-09-11 09:34:53 +0300

    Работа на семинаре

--- Belkin_lab1/Belkin_lab1.cpp
+++ Belkin_lab1/Belkin_lab1.cpp
@@ -1,20 +1,52 @@
-﻿// Belkin_lab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
+using namespace std;
 
-#include <iostream>
+struct Pipe
+{
+    string name;
+    float length;
+    int diametr;
+    bool repair;
+};
+
+struct CS
+{
+    string name;
+    int k_cex;
+    int k_cex_in_work;
+    string class_station;
+};
+
+void Menu(Pipe t)
+{
+    while (1) {
+        cout << "Choose option:\n1. Add pipe\n2. Add CS\n3. View all objects\n";
+        int option;
+        cin >> option;
+        switch (option)
+        {
+        case 1:
+            cout << "Enter name pipe: ";
+            cin >> t.name;
+            break;
+        case 2:
+            cout << "КС добавлена!\n";
+            break;
+        case 3:
+            cout << "Название трубы: " << t.name << endl;
+            break;
+        };
+    };
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
+    Pipe truba;
+    truba.name = "Uchastok-1";
+    cout << truba.name << endl;
+    CS ks;
+    Menu(truba);
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
