dd58fb0a56fd63725471e1942b1c425e9ff93414
dusta <dusta@DESKTOP-ORJ6J08>
2025-09-13 12:49:53 +0300

    Перевод всего меню на русский язык

--- Belkin_lab1/Belkin_lab1.cpp
+++ Belkin_lab1/Belkin_lab1.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <limits>
 #include <fstream>
+#include <clocale>
 using namespace std;
 
 struct Pipe
@@ -24,25 +25,25 @@ struct CS
 void AddPipe(vector<Pipe>& pipes)
 {
     Pipe newPipe;
-    cout << "\nInput name pipe: ";
+    cout << "\nВведите название трубы: ";
     cin >> newPipe.name;
 
-    cout << "Input length pipe: ";
+    cout << "Введите длину трубы (км): ";
     while (!(cin >> newPipe.length) || newPipe.length <= 0) {
-        cout << "Error! Input type: float. And > 0\nInput positive number: ";
+        cout << "Ошибка! Введите ПОЛОЖИТЕЛЬНОЕ ЧИСЛО: ";
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     };
 
-    cout << "Input diametr pipe: ";
+    cout << "Введите диаметр трубы (мм): ";
     while (!(cin >> newPipe.diametr) || newPipe.diametr <= 0) {
-        cout << "Error! Input type: int. And > 0\nInput positive number: ";
+        cout << "Ошибка! Введите ЦЕЛОЕ ПОЛОЖИТЕЛЬНОЕ ЧИСЛО: ";
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     };
     
     string status;
-    cout << "Is the pipe being repaired?(Yes/No): ";
+    cout << "Труба на ремонте?(Yes/No): ";
     while (true) {
         cin >> status;
         if (status == "Yes") {
@@ -54,114 +55,115 @@ void AddPipe(vector<Pipe>& pipes)
             break;
         }
         else {
-            cout << "Error! Input 'Yes' or 'No'\n";
+            cout << "Ошибка! Введите 'Yes' или 'No': ";
         };
     };
 
     pipes.push_back(newPipe);
-    cout << "New pipe add\n\n";
+    cout << "\nНовая труба добавлена\n\n";
 };
 
 void AddCS(vector<CS>& cs_list)
 {
     CS newCS;
-    cout << "\nInput name CS: ";
+    cout << "\nВведите название КС: ";
     cin >> newCS.name;
 
-    cout << "Input number of workshop: ";
+    cout << "Введите общее количество цехов: ";
     while (!(cin >> newCS.k_cex) || newCS.k_cex <= 0) {
-        cout << "Error! Input type: int. And > 0\nInput positive number: ";
+        cout << "Ошибка! Введите ЦЕЛОЕ ПОЛОЖИТЕЛЬНОЕ ЧИСЛО: ";
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     };
 
-    cout << "Input number of workshop in work: ";
+    cout << "Введите количество цехов в работе: ";
     while (!(cin >> newCS.k_cex_in_work) || newCS.k_cex_in_work < 0 || newCS.k_cex_in_work > newCS.k_cex) {
-        cout << "Error! Input type: int. And > 0. And < workshop\nInput positive number: ";
+        cout << "Ошибка! Введите ЦЕЛОЕ ПОЛОЖИТЕЛЬНОЕ ЧИСЛО и МЕНЬШЕЕ общего количества цехов: ";
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     };
 
-    cout << "Input type CS: ";
+    cout << "Введите тип КС: ";
     cin >> newCS.type;
 
     cs_list.push_back(newCS);
-    cout << "New CS add\n\n";
+    cout << "\nНовая КС добавлена\n\n";
 };
 
 void ViewAllObjects(const vector<Pipe>& pipes, const vector<CS>& cs_list)
 {
     if (pipes.empty()) {
-        cout << "\nPipes empty\n";
+        cout << "\nТРУБ НЕТ\n";
     }
     else {
 
-        cout << "\nAll Pipes:\n--------------------------------------\n";
+        cout << "\nВСЕ ТРУБЫ:\n--------------------------------------\n";
 
         for (size_t i = 0; i < pipes.size(); i++) {
-            cout << "Pipe " << i + 1 << endl;
-            cout << "name: " << pipes[i].name << endl;
-            cout << "length: " << pipes[i].length << endl;
-            cout << "diametr: " << pipes[i].diametr << endl;
+            cout << "Труба " << i + 1 << endl;
+            cout << "Название: " << pipes[i].name << endl;
+            cout << "Длина (км): " << pipes[i].length << endl;
+            cout << "Диаметр (мм): " << pipes[i].diametr << endl;
             if (pipes[i].repair == false) {
-                cout << "repair: No\n";
+                cout << "В ремонте: Нет\n";
             }
             else if (pipes[i].repair == true){
-                cout << "repair: Yes\n";
+                cout << "В ремонте: Да\n";
             };
             cout << "--------------------------------------\n";
         };
     };
 
     if (cs_list.empty()) {
-        cout << "CS empty\n\n";
+        cout << "КС НЕТ\n\n";
     }
     else {
 
-        cout << "\nAll CS:\n--------------------------------------\n";
+        cout << "\nВСЕ КС:\n--------------------------------------\n";
 
         for (size_t i = 0; i < cs_list.size(); i++) {
-            cout << "CS " << i + 1 << endl;
-            cout << "name: " << cs_list[i].name << endl;
-            cout << "workshop: " << cs_list[i].k_cex << endl;
-            cout << "workshop in work: " << cs_list[i].k_cex_in_work << endl;
-            cout << "type: " << cs_list[i].type << endl;
-            cout << "--------------------------------------\n\n";
+            cout << "КС " << i + 1 << endl;
+            cout << "Название: " << cs_list[i].name << endl;
+            cout << "Общее количество цехов: " << cs_list[i].k_cex << endl;
+            cout << "Количество цехов в работе: " << cs_list[i].k_cex_in_work << endl;
+            cout << "Тип: " << cs_list[i].type << endl;
+            cout << "--------------------------------------\n";
         };
+        cout << endl;
     };
 };
 
 void EditPipe(vector<Pipe>& pipes)
 {   
     if (pipes.empty()) {
-        cout << "\nPipes empty\n\n";
+        cout << "\nТРУБ НЕТ\n\n";
         return;
     }
 
     int i;
-    cout << "\nInput pipe number: ";
+    cout << "\nВведите номер трубы: ";
     while (!(cin >> i) || i <= 0 ||  i > pipes.size()) {
-        cout << "Error! Input type: int. And > 0. And < size pipes\nInput positive number: ";
+        cout << "Ошибка! Введите ЦЕЛОЕ ПОЛОЖИТЕЛЬНОЕ ЧИСЛО не превышающее общего количества труб: ";
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     };
 
     string status;
-    cout << "\nIs the pipe being repaired?(Yes/No): ";
+    cout << "\nТруба в ремонте?(Yes/No): ";
     while (true) {
         cin >> status;
         if (status == "Yes") {
             pipes[i - 1].repair = true;
-            cout << "Status change in true\n\n";
+            cout << "Статус 'в ремонте' сменен на 'Да'\n\n";
             break;
         }
         else if (status == "No") {
             pipes[i - 1].repair = false;
-            cout << "Status change in false\n\n";
+            cout << "Статус 'в ремонте' сменен на 'Нет'\n\n";
             break;
         }
         else {
-            cout << "Error! Input 'Yes' or 'No'\n";
+            cout << "Ошибка! Введите 'Yes' или 'No': ";
         };
     };
 };
@@ -169,28 +171,28 @@ void EditPipe(vector<Pipe>& pipes)
 void EditCS(vector<CS>& cs_list)
 {
     if (cs_list.empty()) {
-        cout << "\nCS empty\n\n";
+        cout << "\nКС НЕТ\n\n";
         return;
     };
 
     int i;
-    cout << "\nInput CS number: ";
+    cout << "\nВведите номер КС: ";
     while (!(cin >> i) || i <= 0 || i > cs_list.size()) {
-        cout << "Error! Input type: int. And > 0. And < size cs_list\nInput positive number: ";
+        cout << "Ошибка! Введите ЦЕЛОЕ ПОЛОЖИТЕЛЬНОЕ ЧИСЛО не превышающее общего количества КС: ";
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     };
 
     while (1) {
         cout << "\n--------------------------------------\n";
-        cout << "CS " << i << endl;
-        cout << "works: " << cs_list[i - 1].k_cex_in_work << "/" << cs_list[i - 1].k_cex << endl;
-        cout << "\nChoose option:\n1. Run workshop +1\n2. Stop workshop -1\n3. Exit\n";
+        cout << "КС " << i << endl;
+        cout << "В работе: " << cs_list[i - 1].k_cex_in_work << "/" << cs_list[i - 1].k_cex << endl;
+        cout << "\nВыберите опцию:\n1. Запустить цех (+1)\n2. Остановить цех (-1)\n3. Выход\n";
         cout << "--------------------------------------\n\n";
         int option;
 
         while (!(cin >> option)) {
-            cout << "Error type.\nInput positive number: ";
+            cout << "Ошибка! Введите ЦЕЛОЕ число: ";
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
         };
@@ -202,7 +204,7 @@ void EditCS(vector<CS>& cs_list)
                 cs_list[i - 1].k_cex_in_work++;
             }
             else {
-                cout << "\nThe number of workshop workers cannot be greater than the total number of workshops.\n";
+                cout << "\nВсе цеха уже в работе!\n";
             };
             break;
         case 2:
@@ -210,14 +212,14 @@ void EditCS(vector<CS>& cs_list)
                 cs_list[i - 1].k_cex_in_work--;
             }
             else {
-                cout << "\nThe number of workshop workers cannot be less than zero.\n";
+                cout << "\nВсе цеха уже остановлены!\n";
             };
             break;
         case 3:
-            cout << "\nExiting...\n\n";
+            cout << "\nВыходим...\n\n";
             return;
         default:
-            cout << "Invalid option! Try again.\n";
+            cout << "Неизвестная опция. Попробуйте еще раз!\n";
             break;
         };
     };
@@ -233,12 +235,12 @@ void Save(const vector<Pipe>& pipes, const vector<CS>& cs_list)
             for (size_t i = 0; i < pipes.size(); i++) {
                 pipes_save << pipes[i].name << "|" << pipes[i].length << "|" << pipes[i].diametr << "|" << pipes[i].repair << "|" << endl;
             };
-            cout << "\nPipes are saved!\n";
+            cout << "\nСписок труб сохранен!\n";
         };
         pipes_save.close();
     }
     else {
-        cout << "\nPipes empty\n";
+        cout << "\nТРУБ НЕТ\n";
     };
 
     if (!(cs_list.empty())) {
@@ -250,11 +252,11 @@ void Save(const vector<Pipe>& pipes, const vector<CS>& cs_list)
                 cs_save << cs_list[i].name << "|" << cs_list[i].k_cex << "|" << cs_list[i].k_cex_in_work << "|" << cs_list[i].type << "|" << endl;
             };
         };
-        cout << "CS are saved!\n\n";
+        cout << "Список КС сохранен!\n\n";
         cs_save.close();
     }
     else {
-        cout << "CS empty\n\n";
+        cout << "КС НЕТ\n\n";
     };
 };
 
@@ -297,11 +299,11 @@ void Upload(vector<Pipe>& pipes, vector<CS>& cs_list)
 
             pipes.push_back(newPipe);
         };
-        cout << "\nPipes are uploaded!\n";
+        cout << "\nСписок труб загружен!\n";
         pipes_upload.close();
     }
     else {
-        cout << "\nFile 'pipes.txt' not found\n";
+        cout << "\nФайл 'pipes.txt' не найден\n";
         pipes_upload.close();
     };
 
@@ -333,11 +335,11 @@ void Upload(vector<Pipe>& pipes, vector<CS>& cs_list)
 
             cs_list.push_back(newCS);
         };
-        cout << "CS are uploaded!\n\n";
+        cout << "Список КС загружен!\n\n";
         cs_upload.close();
     }
     else {
-        cout << "File 'cs_list.txt' not found\n\n";
+        cout << "Файл 'cs_list.txt' не найден\n\n";
         cs_upload.close();
     };
 };
@@ -346,12 +348,12 @@ void Menu(vector<Pipe>& pipes, vector<CS>& cs_list)
 {
     while (1) {
         cout << "--------------------------------------\n";
-        cout << "Choose option:\n1. Add Pipe\n2. Add CS\n3. View all objects\n4. Edit pipe\n5. Edit CS\n6. Save\n7. Upload\n0. Exit\n";
+        cout << "Выберите опцию:\n1. Добавить трубу\n2. Добавить КС\n3. Просмотр всех объектов\n4. Редактировать трубу\n5. Редактировать КС\n6. Сохранить\n7. Загрузить\n0. Выход\n";
         cout << "--------------------------------------\n\n";
         int option;
 
         while (!(cin >> option)) {
-            cout << "Error type.\nInput positive number: ";
+            cout << "Ошибка. Введите ЦЕЛОЕ число: ";
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
         };
@@ -379,10 +381,10 @@ void Menu(vector<Pipe>& pipes, vector<CS>& cs_list)
             Upload(pipes, cs_list);
             break;
         case 0:
-            cout << "\nExiting...\n";
+            cout << "\nВыходим...\n";
             return;
         default:
-            cout << "Invalid option! Try again.\n";
+            cout << "Неизвестная опция. Попробуйте еще раз\n\n";
             break;
         };
     };
@@ -390,6 +392,7 @@ void Menu(vector<Pipe>& pipes, vector<CS>& cs_list)
 
 int main()
 {
+    setlocale(LC_ALL, "");
     vector<Pipe> pipes;
     vector<CS> cs_list;
     Menu(pipes, cs_list);
